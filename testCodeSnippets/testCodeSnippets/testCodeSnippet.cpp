fac1cea298fcf8a180697720163f8fde62dbc7f9
ronnie wong <rjwrobotics@gmail.com>
2018-01-17 23:17:23 -0500

    adding visual studio testSnippet solution

---
+++ testCodeSnippets/testCodeSnippets/testCodeSnippet.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	printf("Hello World!\n\r");
+	printf("Press any key to exit()\n\r");
+	getchar();
+
+	return 0;
+}
