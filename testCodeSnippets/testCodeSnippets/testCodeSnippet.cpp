0d32a238fac590ce11c5b55603befe73f264d5c8
robopup <ronnie.wong@geotech.ca>
2018-01-18 13:46:01 -0500

    added array sort and little endian rearrangement - working

--- testCodeSnippets/testCodeSnippets/testCodeSnippet.cpp
+++ testCodeSnippets/testCodeSnippets/testCodeSnippet.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 #include <stdlib.h>
 #include <Windows.h>
+#include <string>
 
+void swap(char *firstElem, char *secondElem);
 
 using namespace std;
 
 int main()
 {
 	char someString[] = "Hello Skunk Works. What are you building?";
-	char dataVals[9] = { 0x12, 0x23, 0x34, 0x45, 0x56, 0x67, 0x78, 0x89, 0 };
-	DWORD newArray = 0;
+	char dataVals[9] = { 0xF8, 0x14, 0x45, 0xFE, 0x56, 0x67, 0x78, 0x89, 0 };
+	char packetResults[8];
+	int newArray = 0x00000000;
+	int assembled = 0x00000000;
+	int dataResults[2];
+	int wrapSize = 4;
+
 
 	printf("Hello World!\n\r");
 	printf("Press any key to exit()\n\r");
@@ -26,16 +33,71 @@ int main()
 	printf("Press any key to continue()\n\r");
 	getchar();
 
+	// Rearrange buffer array
+	for (int i = 0; i < 8; i+=4) {
+		swap(dataVals[i + 0], dataVals[i + 3]);
+		swap(dataVals[i + 1], dataVals[i + 2]);
+	}
+
+	for (int i = 0; i < sizeof(dataVals); i++) {
+		printf("dataVals_Hex[%d]: %02X (%d)\n\r", i, (unsigned char)dataVals[i], (unsigned char)dataVals[i], (unsigned char)dataVals[i]);
+	}
+	printf("Press any key to continue()\n\r");
+	getchar();
+
 	// Combine array of values in Hexidecimal!
-	newArray = dataVals[0];
-	newArray = (newArray << 8) + dataVals[1];
-	printf("newArray is: %032X\n\r", newArray);
+	for (int j = 0; j < wrapSize; j++) {
+		assembled = (assembled << 8)|(unsigned char)dataVals[j];
+		printf("assembled after %d:    %08X\n\r", j, assembled);
+	}
+	printf("assembled array is: %08X\n\r", assembled);
+	printf("The decimal equivalent of assembled is: %ld\n\r", assembled);
 	printf("Press any key to exit()\n\r");
 	getchar();
 
-	for (int j = 0; j < (sizeof(dataVals) - 1); j++) {
-
+	// Combine arrays into 32bit DWORD
+	for (int x = 0; x < 2; x++) {
+		int track = 0;
+		for (int k = 0; k < 8; k += 4) {
+			for (int m = 0; m < wrapSize; m++) {
+				dataResults[track] = (dataResults[track] << 8) | (unsigned char)dataVals[m + k];
+			}
+			track++;
+		}
 	}
 
+	/*
+	for (int k = 0; k < 2; k++) {
+		printf("dataResult[%d]: %08X\n\r", k, dataResults[k]);
+	}
+	getchar();
+
+	for (int k = 0; k < 2; k++) {
+		dataResults[k] = 0;
+	}
+
+	track = 0;
+	for (int k = 0; k < 8; k += 4) {
+		for (int m = 0; m < wrapSize; m++) {
+			dataResults[track] = (dataResults[track] << 8) | (unsigned char)dataVals[m + k];
+		}
+		track++;
+	}
+	*/
+
+
+	for (int k = 0; k < 2; k++) {
+		printf("dataResult[%d]: %08X\n\r", k, dataResults[k]);
+	}
+	getchar();
+	
 	return 0;
+}
+
+void swap(char *firstElem, char *secondElem)
+{
+	char temp = *firstElem;
+
+	*firstElem = *secondElem;
+	*secondElem = temp;
 }
